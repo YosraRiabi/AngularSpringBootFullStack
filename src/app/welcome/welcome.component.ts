af0e1cc28afe709c40dd531dc311cca44c753a79
YosraRiabi <riabiyosraa@gmail.com>
2019-02-05 12:12:55 +0100

    Some differences between Angular & Java

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,27 @@
+// package com.riabi.springboot.web;
+// import org.springframework.boot.SpringApplication;
 import { Component, OnInit } from '@angular/core';
 
+// @ComponentScan (
+//         value = "com.riabi.springboot.web")
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+// public class SprinBootFirstWebApplication implements SomeInterface {
 export class WelcomeComponent implements OnInit {
 
+  // String message = "some welcome message";
+  message = 'some welcome message';
+
+  // public SprinBootFirstWebApplication() {
   constructor() { }
 
+  // void init () {
   ngOnInit() {
+    console.log(this.message);
   }
 
 }
