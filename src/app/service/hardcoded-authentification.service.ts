5359681f21c2247c39770952c760de93c2eaa6d6
YosraRiabi <riabiyosraa@gmail.com>
2019-02-06 14:46:38 +0100

    Using session storage to Store User Authentication Token

--- src/app/service/hardcoded-authentification.service.ts
+++ src/app/service/hardcoded-authentification.service.ts
@@ -8,8 +8,16 @@ export class HardcodedAuthentificationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log('before ' + this.isUserLoggedIn());
     if (username === 'yosra' && password === 'riabi') {
+      sessionStorage.setItem('authentificaterUser', username);
+      console.log('after ' + this.isUserLoggedIn());
       return true;
     } else { return false; }
+  }
+
+  isUserLoggedIn() {
+   const user = sessionStorage.getItem('authentificaterUser');
+   return !(user === null);
   }
 }
