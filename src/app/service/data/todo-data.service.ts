8d144c2e3445fa26ff8c79fd224ec77a535b4858
YosraRiabi <riabiyosraa@gmail.com>
2019-02-12 14:06:59 +0100

    Creating Retrieve Todo Service and Connect Angular Frontend

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -17,7 +17,11 @@ export class TodoDataService {
     // console.log('execute hello world bean service');
   }
 
-  deleteTodo(username, id){
+  deleteTodo(username, id) {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+
+  retrieveTodo(username, id) {
+    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
   }
 }
